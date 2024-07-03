2517cef0cf2bca38a01e9c39e6ec90be506d66df
Tommy H Yeargin Jr <yeargin2021@gmail.com>
2024-07-03 12:34:15 -0400

    The dice is rolling! ;)

--- display.py
+++ display.py
@@ -1,3 +1,9 @@
-with open('die_side_06', 'r', encoding='utf-8') as file:
+from randomDancing import rollWithIt
+
+theDieSideUp = 'die_side_0' + str(rollWithIt())
+
+with open(theDieSideUp, 'r', encoding='utf-8') as file:
     for i in file:
         print(i, end='')
+
+# print(theDieSideUp) ----> This last line is unnecessary.
---
+++ randomDancing.py
@@ -0,0 +1,7 @@
+def rollWithIt():
+    from random import randint
+    return (randint(1, 6))
+
+
+if __name__ == '__main__':
+    rollWithIt()
